2fa2a60293bd7d34d8b44f21c6abadffb8dd3a2c
alffox <alfonso.crisci.it@gmail.com>
2019-07-09 10:07:38 +0200

    Refactor: Add DEV custom title and color

--- src/modules/AtwHeader.js
+++ src/modules/AtwHeader.js
@@ -8,7 +8,7 @@ class AtwHeader extends React.Component {
             <header className="row">
                 <div className="col-md-12 text-center text-uppercase">
                     <img className="float-left img-fluid favicon" src={logo} alt="Paper airplane icon" />
-                    <h1 className="title-super text-muted position-relative d-none d-sm-block">Liferay Around the World</h1>
+                    <h1 className="title-super text-muted position-relative d-none d-sm-block">ReactJS - Liferay Around the World</h1>
                 </div>
             </header>
             )
