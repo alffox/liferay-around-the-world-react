abdc66dd4542f8317e59926ff0415ba1857d1e60
alffox <alfonso.crisci.it@gmail.com>
2019-07-12 13:53:20 +0200

    feat: Get data for clicked location

--- src/modules/AtwFlags.js
+++ src/modules/AtwFlags.js
@@ -5,10 +5,19 @@ class AtwFlags extends React.Component {
 
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+        
         this.state = {
-            count: 0
+            city: locationsData.locations[0].title
         };
     }
+
+    handleClick = (selCity) => {
+        console.log(selCity);
+        this.setState({
+            city: selCity
+          });
+      }
 	  
 	render() {
         return (
@@ -17,7 +26,7 @@ class AtwFlags extends React.Component {
                     <h3 className="location-tags d-none d-xl-block">
                         {locationsData.locations.map ((location, index) => {
 				            return (
-                                <button key={index} type="button" className="btn btn-info m-1 flag-top">
+                                <button onClick={this.handleClick.bind(this,location.title)} key={index} type="button" className="btn btn-info m-1 flag-top">
                                     <div className={location.ISO_3166_1_alpha_2 + " flag-button mx-auto mr-3"}></div>{location.title}, {location.country}
                                 </button>
                             );
