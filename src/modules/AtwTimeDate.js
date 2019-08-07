2e8f01c0631f02261afe2d6d1fb0e0166e7a7653
alffox <alfonso.crisci.it@gmail.com>
2019-08-07 11:42:01 +0200

    refactor: Use unique modules for API calls

--- src/modules/AtwTimeDate.js
+++ src/modules/AtwTimeDate.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import AtwAPICall from "./AtwAPICall.js";
+import axios from 'axios';
+
+import { locationsData } from '../App.js';
+
+const URL = process.env.REACT_APP_REST_API_SERVER + '/TimeDateEndpoint?format=json&by=zone&zone=' + locationsData.locations[0].timezone_database_name;
 
 class AtwTimeDate extends React.Component {
 	  
-	render() {
+    state = {
+        timeDate: null,
+        time: null,
+        date: null
+    }
+
+componentDidMount() {
+            axios.get(URL).then(response => response.data)
+            .then((data) => {
+            this.setState({
+                timeDate: data,
+                date: data.formatted.substr(0,data.formatted.indexOf(' ')),
+                time : data.formatted.substr(data.formatted.indexOf(' ')+1)
+            })
+        })
+    }
+
+    render() {
         return (
-            <div className="row">
-                        <AtwAPICall />
+            <div className="col-md-12 text-info font-weight-bold text-center font-italic">
+                <div className="timedate">
+                    <i className="far fa-calendar-alt"></i><span className="date px-1">{this.state.date}</span>
+                    <i className="far fa-clock"></i><span className="time px-1">{this.state.time}</span>
+                </div>
             </div>
-            )
-        }
+        )
+      }
     }
     
 export default AtwTimeDate;
