2c10c4cf36a558a048a862a00bd66af93ac7e884
alffox <alfonso.crisci.it@gmail.com>
2019-08-07 11:29:04 +0200

    Refactor: Remove dummy API call

--- src/App.js
+++ src/App.js
@@ -6,47 +6,47 @@ import AtwHeader from "./modules/AtwHeader.js";
 import AtwFlags from "./modules/AtwFlags.js";
 import AtwTimeDate from "./modules/AtwTimeDate.js";
 
-const API = 'https://en.wikipedia.org/api/rest_v1/page/summary/Italy'; 
+// const API = 'https://en.wikipedia.org/api/rest_v1/page/summary/Italy'; 
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      error: null,
-      isLoaded: false,
-      description: []
-    };
-  }
+//   constructor() {
+//     super();
+//     this.state = {
+//       error: null,
+//       isLoaded: false,
+//       description: []
+//     };
+//   }
 
-  componentDidMount() {
-    fetch(API)
-    .then(response =>  response.json())
-    .then(resData => {
-       //console.log(JSON.stringify(resData))
-       //do your logic here       
-       //let person = resData.results
-       this.setState({
-        isLoaded: true,
-        description: resData.extract
-      }); //this is an asynchronous function
-    })
-    .catch(
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
-  }
+//   componentDidMount() {
+//     fetch(API)
+//     .then(response =>  response.json())
+//     .then(resData => {
+//        //console.log(JSON.stringify(resData))
+//        //do your logic here       
+//        //let person = resData.results
+//        this.setState({
+//         isLoaded: true,
+//         description: resData.extract
+//       }); //this is an asynchronous function
+//     })
+//     .catch(
+//       (error) => {
+//         this.setState({
+//           isLoaded: true,
+//           error
+//         });
+//       }
+//     )
+//   }
 
   render() {
-    const { error, isLoaded } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
+    // const { error, isLoaded } = this.state;
+    // if (error) {
+    //   return <div>Error: {error.message}</div>;
+    // } else if (!isLoaded) {
+    //   return <div>Loading...</div>;
+    // } else {
       return (
         <div className="container-fluid">
         <AtwHeader />
@@ -55,8 +55,8 @@ class App extends React.Component {
         </div>
       );
     }
-  }
-}
+   }
+// }
 
 export {locationsData};
 export default App;
